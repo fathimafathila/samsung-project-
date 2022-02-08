27b58f43a35294d5549ba58c9c9255854a287efd
BasilDhilloo <bdhilloo@gmail.com>
2022-02-07 22:01:18 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/covidwatch/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.covidwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+//    Button btn = (Button)findViewById(R.id.cancel_button);
+//    btn.setOnc(new View.OnClickListener() {
+//        public void onClick(View v) {
+//            startActivity(new Intent(this, AdminDashboard.class));
+//        }
+//    });
+}
