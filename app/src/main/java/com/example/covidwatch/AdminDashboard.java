aec0ce7a03ca1465f2462225de3ca35344abb96d
BasilDhilloo <bdhilloo@gmail.com>
2022-02-12 18:21:42 -0400

    Admin Onclick on CardView Added

--- app/src/main/java/com/example/covidwatch/AdminDashboard.java
+++ app/src/main/java/com/example/covidwatch/AdminDashboard.java
@@ -2,13 +2,36 @@ package com.example.covidwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.card.MaterialCardView;
+
 
 public class AdminDashboard extends AppCompatActivity {
+    private MaterialCardView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
-    }
+
+        tv = (MaterialCardView) this.findViewById(R.id.newRecord);
+
+        // Method for admin dashboard
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDashboardAct();
+                }
+            });
+        }
+
+        public void openDashboardAct(){
+            Intent intent = new Intent(this, TestActivity.class);
+            startActivity(intent);
+        }
 }
