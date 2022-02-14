af0448534536bac1d55e7690d09da617dc89776f
Shrinal Patel <shrinalpatel98@gmail.com>
2022-02-13 21:53:54 -0500

    validated login & security page

--- app/src/main/java/com/example/covidwatch/MainActivity.java
+++ app/src/main/java/com/example/covidwatch/MainActivity.java
@@ -4,30 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    EditText edtUsername, edtPassword;
+    boolean isAllFieldsChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Method for security question
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
         button = (Button)findViewById(R.id.btnLogin);
+
+        // Method for opening security question page
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDashboardAct();
+
+                isAllFieldsChecked = CheckAllFields();
+
+                if (isAllFieldsChecked) {
+                    Intent i = new Intent(MainActivity.this, SecurityQuestionActivity.class);
+                    startActivity(i);
+                }
             }
-        });
+
+            });
     }
 
-    public void openDashboardAct(){
-        Intent intent = new Intent(this, SecurityQuestionActivity.class);
-        startActivity(intent);
+    private boolean CheckAllFields() {
+        if (edtUsername.length() == 0) {
+            edtUsername.setError("Username should not be empty");
+            return false;
+        }
+
+        if (edtPassword.length() == 0) {
+            edtPassword.setError("Password should not be empty");
+            return false;
+        } else if (edtPassword.length() != 6) {
+            edtPassword.setError("Password must be of 6 characters");
+            return false;
+        }
+
+        // after all validation return true.
+        return true;
     }
 }
--- app/src/main/java/com/example/covidwatch/SecurityQuestionActivity.java
+++ app/src/main/java/com/example/covidwatch/SecurityQuestionActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SecurityQuestionActivity extends AppCompatActivity {
     private Button button;
+    EditText edtSecurity;
+    boolean isAllFieldsChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +20,18 @@ public class SecurityQuestionActivity extends AppCompatActivity {
 
         // Method for admin dashboard
         button = (Button)findViewById(R.id.btnProceed);
+        edtSecurity = findViewById(R.id.edtSecurity);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSecurityPage();
+
+                isAllFieldsChecked = CheckAllFields();
+
+                if (isAllFieldsChecked) {
+                    openSecurityPage();
+                }
+
             }
         });
     }
@@ -28,5 +39,15 @@ public class SecurityQuestionActivity extends AppCompatActivity {
     public void openSecurityPage(){
         Intent intent = new Intent(this, AdminDashboard.class);
         startActivity(intent);
+    }
+
+    private boolean CheckAllFields() {
+        if (edtSecurity.length() == 0) {
+            edtSecurity.setError("Security answer should not be empty");
+            return false;
+        }
+
+        // after all validation return true.
+        return true;
     }
 }
