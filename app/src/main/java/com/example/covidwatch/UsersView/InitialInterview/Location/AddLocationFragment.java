404665cbd010cc753efb7a01eb5f798bb6ebe7b5
Shrinal Patel <shrinalpatel98@gmail.com>
2022-03-21 21:05:14 -0400

    updated location

--- app/src/main/java/com/example/covidwatch/UsersView/InitialInterview/Location/AddLocationFragment.java
+++ app/src/main/java/com/example/covidwatch/UsersView/InitialInterview/Location/AddLocationFragment.java
@@ -2,6 +2,12 @@ package com.example.covidwatch.UsersView.InitialInterview.Location;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,19 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.covidwatch.R;
-import com.example.covidwatch.UsersView.InitialInterview.Location.ContactRecycle.ContactModel;
-import com.example.covidwatch.UsersView.InitialInterview.Location.ContactRecycle.contactAdapter;
 import com.example.covidwatch.UsersView.InitialInterview.Location.LocationRecycle.LocationModel;
 import com.example.covidwatch.UsersView.InitialInterview.Location.LocationRecycle.locationAdapter;
 
@@ -65,7 +59,7 @@ public class AddLocationFragment extends Fragment {
         //Create adapter
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.arrContactType, R.layout.list_item);
+                R.array.arrLocationType, R.layout.list_item);
 
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
         spinnerContactType.setAdapter(adapter);
